15aaa8fffaed8a5d3181536eba2179d345f4d6e9
Daria Havlovych <daria.havlovych@gmail.com>
2024-09-26 14:42:38 +0300

    add reviews

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchReviewsByMovieId } from "../../services/api";
+
 const MovieReviews = () => {
+  const { movieId } = useParams();
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    const getReviews = async () => {
+      const data = await fetchReviewsByMovieId(movieId);
+      setReviews(data);
+    };
+    getReviews();
+  }, [movieId]);
+
+  if (reviews.length === 0) {
+    return <h2>loading...</h2>;
+  }
+
   return (
     <div>
-      <p>here will be reviews</p>
+      <ul>
+        {reviews.map((review) => (
+          <li key={review.id}>
+            <p>{review.author}</p>
+            <p>{review.content}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
